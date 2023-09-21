e9c2a7543a42de3cfb4facda54c77265e49cc0ac
Aurèle Oulès <aurele@oules.com>
2023-09-21 14:13:31 +0000

    Do not crash if PR is already merged

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,27 +16,30 @@ gcovr --json --gcov-executable "llvm-cov gcov" --gcov-ignore-parse-errors -e dep
 aws s3 cp coverage.json s3://bitcoin-coverage-data/$PR_NUM/coverage.json
 
 changed_files=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
-changed_files=$(echo "$changed_files" | grep -E "^src/")
-changed_files=$(echo "$changed_files" | grep -vE "^src/(test|wallet/test)/")
-changed_files=$(echo "$changed_files" | grep -E "\.(cpp|h)$")
-changed_files=$(echo "$changed_files" | tr '\n' ' ')
-changed_files=$(echo "$changed_files" | sed 's/ $//g')
-# remove src/ from each file
-changed_files_base=$(echo "$changed_files" | sed 's/src\///g')
 
-# for each file create an array of object like [{"name": "spend.cpp"}, {"name": ...]
-filter="[{\"name\": \"$(echo "$changed_files_base" | sed 's| |\"}, {\"name\": \"|g')\"}]"
+if [ -n "$changed_files" ]; then
+    changed_files=$(echo "$changed_files" | grep -E "^src/")
+    changed_files=$(echo "$changed_files" | grep -vE "^src/(test|wallet/test)/")
+    changed_files=$(echo "$changed_files" | grep -E "\.(cpp|h)$")
+    changed_files=$(echo "$changed_files" | tr '\n' ' ')
+    changed_files=$(echo "$changed_files" | sed 's/ $//g')
+    # remove src/ from each file
+    changed_files_base=$(echo "$changed_files" | sed 's/src\///g')
 
-mutators=$(clang-tidy -load /usr/lib/libbitcoin-mutator.so --list-checks --checks=mutator-* | grep "mutator-")
+    # for each file create an array of object like [{"name": "spend.cpp"}, {"name": ...]
+    filter="[{\"name\": \"$(echo "$changed_files_base" | sed 's| |\"}, {\"name\": \"|g')\"}]"
 
-mkdir /tmp/mutations
-for mutator in $mutators; do
-    echo "clang-tidy -load /usr/lib/libbitcoin-mutator.so --checks=$mutator --line-filter='$filter' --export-fixes=/tmp/mutations/$mutator.yml $changed_files" >> commands.txt
-done
+    mutators=$(clang-tidy -load /usr/lib/libbitcoin-mutator.so --list-checks --checks=mutator-* | grep "mutator-")
 
-parallel --jobs $(nproc) < commands.txt || true
-sed -i 's|/tmp/bitcoin/||g' /tmp/mutations/*.yml
+    mkdir /tmp/mutations
+    for mutator in $mutators; do
+        echo "clang-tidy -load /usr/lib/libbitcoin-mutator.so --checks=$mutator --line-filter='$filter' --export-fixes=/tmp/mutations/$mutator.yml $changed_files" >> commands.txt
+    done
 
-cd /tmp/mutations && zip -r /tmp/mutations.zip *
+    parallel --jobs $(nproc) < commands.txt || true
+    sed -i 's|/tmp/bitcoin/||g' /tmp/mutations/*.yml
 
-aws s3 cp /tmp/mutations.zip s3://bitcoin-coverage-data/$PR_NUM/mutations.zip
+    cd /tmp/mutations && zip -r /tmp/mutations.zip *
+
+    aws s3 cp /tmp/mutations.zip s3://bitcoin-coverage-data/$PR_NUM/mutations.zip
+fi
