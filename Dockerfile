d21fa7fee5e8a14a04680da4c97b431ce7a09cf4
Aurele Oules <aurele@oules.com>
2023-11-25 16:10:31 +0100

    Remove mutation testing and rollback to gcc

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,8 @@
 FROM ubuntu:22.04
 
 ENV DEBIAN_FRONTEND=noninteractive
-RUN apt update && apt install -y git python3-zmq libevent-dev libboost-dev libdb5.3++-dev libsqlite3-dev libminiupnpc-dev libzmq3-dev lcov libtool autotools-dev automake pkg-config bsdmainutils bsdextrautils curl wget python3-pip lsb-release software-properties-common gnupg unzip jq parallel zip vim htop
+RUN apt update && apt install -y git python3-zmq libevent-dev libboost-dev libdb5.3++-dev libsqlite3-dev libminiupnpc-dev libzmq3-dev lcov build-essential libtool autotools-dev automake pkg-config bsdmainutils bsdextrautils curl wget python3-pip lsb-release software-properties-common gnupg unzip jq parallel zip vim htop
 RUN pip install gcovr compiledb
-RUN wget https://apt.llvm.org/llvm.sh && chmod +x llvm.sh && ./llvm.sh 16
-RUN apt install -y clang-tidy-16 clang-16
-RUN update-alternatives --install /usr/bin/clang clang /usr/bin/clang-16 100
-RUN update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-16 100
-RUN update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-16 100
-RUN update-alternatives --install /usr/bin/llvm-cov llvm-cov /usr/bin/llvm-cov-16 100
 RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
     unzip awscliv2.zip && \
     ./aws/install && \
@@ -16,11 +10,6 @@ RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2
 
 RUN git config --global user.email "bitcoin-coverage@aureleoules.com"
 RUN git config --global user.name "bitcoin-coverage"
-
-RUN wget https://nightly.link/corecheck/chernobyl/workflows/cmake-single-platform/master/libbitcoin-mutator.so.zip?h=c25bdfbe7b86210a96437322d9633c6003aebba5 -O libbitcoin-mutator.so.zip && \
-    unzip libbitcoin-mutator.so.zip && \
-    mv libbitcoin-mutator.so /usr/lib/ && \
-    rm -rf libbitcoin-mutator.so.zip
 
 RUN wget https://github.com/mozilla/sccache/releases/download/v0.5.4/sccache-v0.5.4-x86_64-unknown-linux-musl.tar.gz && \
     tar -xvf sccache-v0.5.4-x86_64-unknown-linux-musl.tar.gz && \
@@ -36,10 +25,9 @@ RUN wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-s
 
 RUN git clone https://github.com/bitcoin/bitcoin.git /tmp/bitcoin
 WORKDIR /tmp/bitcoin
-RUN CC=clang CXX=clang++ make -C depends NO_BOOST=1 NO_LIBEVENT=1 NO_QT=1 NO_SQLITE=1 NO_NATPMP=1 NO_UPNP=1 NO_ZMQ=1 NO_USDT=1
+RUN make -C depends NO_BOOST=1 NO_LIBEVENT=1 NO_QT=1 NO_SQLITE=1 NO_NATPMP=1 NO_UPNP=1 NO_ZMQ=1 NO_USDT=1
 ENV BDB_PREFIX=/tmp/bitcoin/depends/x86_64-pc-linux-gnu
 RUN mkdir -p /tmp/bitcoin/releases && ./test/get_previous_releases.py -b
-
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,15 +14,15 @@ sed -i 's|$(LCOV) -z $(LCOV_OPTS) -d $(abs_builddir)/src||g' ./Makefile.am
 
 # create function 
 function configure_and_compile() {
-    ./autogen.sh && CXX=clang++ CC=clang ./configure --disable-fuzz --enable-fuzz-binary=no --with-gui=no --disable-zmq --disable-bench BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --enable-lcov #--enable-extended-functional-tests
+    ./autogen.sh && ./configure --disable-fuzz --enable-fuzz-binary=no --with-gui=no --disable-zmq --disable-bench BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --enable-lcov #--enable-extended-functional-tests
     compiledb make -j$(nproc)
 }
 
 configure_and_compile
 make cov
 
-gcovr --json --gcov-ignore-errors=no_working_dir_found --gcov-executable "llvm-cov gcov" --gcov-ignore-parse-errors -e depends -e src/test -e src/leveldb -e src/bench -e src/qt > coverage.json
-aws s3 cp coverage.json s3://bitcoin-coverage-data/$PR_NUM/coverage.json
+gcovr --json --gcov-ignore-errors=no_working_dir_found --gcov-ignore-parse-errors -e depends -e src/test -e src/leveldb -e src/bench -e src/qt > coverage.json
+aws s3 cp coverage.json s3://bitcoin-coverage-data/$PR_NUM/$(git rev-parse HEAD)/coverage.json
 
 last_master_commit=$(curl "https://sonarcloud.io/api/project_analyses/search?project=aureleoules_bitcoin&branch=master" | jq -r '.analyses[0].revision')
 # check if the last master commit is the same as the current master commit
@@ -65,35 +65,3 @@ echo "Updating $PR_NUM branch on sonarcloud"
     -Dsonar.branch.name=$PR_NUM \
     -Dsonar.cfamily.analysisCache.mode=server \
     -Dsonar.branch.target=master
-
-set +e
-changed_files=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
-
-if [ -n "$changed_files" ]; then
-    changed_files=$(echo "$changed_files" | grep -E "^src/")
-    changed_files=$(echo "$changed_files" | grep -vE "^src/(test|wallet/test)/")
-    changed_files=$(echo "$changed_files" | grep -E "\.(cpp|h)$")
-    changed_files=$(echo "$changed_files" | tr '\n' ' ')
-    changed_files=$(echo "$changed_files" | sed 's/ $//g')
-    # remove src/ from each file
-    changed_files_base=$(echo "$changed_files" | sed 's/src\///g')
-
-    if [ -n "$changed_files_base" ]; then
-        # for each file create an array of object like [{"name": "spend.cpp"}, {"name": ...]
-        filter="[{\"name\": \"$(echo "$changed_files_base" | sed 's| |\"}, {\"name\": \"|g')\"}]"
-
-        mutators=$(clang-tidy -load /usr/lib/libbitcoin-mutator.so --list-checks --checks=mutator-* | grep "mutator-")
-
-        mkdir /tmp/mutations
-        for mutator in $mutators; do
-            echo "clang-tidy -load /usr/lib/libbitcoin-mutator.so --checks=$mutator --line-filter='$filter' --export-fixes=/tmp/mutations/$mutator.yml $changed_files" >> commands.txt
-        done
-
-        parallel --jobs $(nproc) < commands.txt || true
-        sed -i 's|/tmp/bitcoin/||g' /tmp/mutations/*.yml
-
-        cd /tmp/mutations && zip -r /tmp/mutations.zip * || true
-
-        aws s3 cp /tmp/mutations.zip s3://bitcoin-coverage-data/$PR_NUM/mutations.zip || true
-    fi
-fi
